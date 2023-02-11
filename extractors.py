68b86b2a1bea5028476d7fcecaf0dda77c1ac469
headcodeti <rodolfo@head-code.com.br>
2023-02-11 13:46:33 -0300

    fix header 2012

--- extractors.py
+++ extractors.py
@@ -668,8 +668,10 @@ class VotacaoZonaExtractor(Extractor):
         uf = self.extract_state_from_filename(internal_filename)
         if year < 2010:
             header_year = "1994"
-        elif 2010 <= year <= 2012:
+        elif 2010 <= year <= 2010:
             header_year = "2010"
+        elif 2012 <= year <= 2012:
+            header_year = "2012"    
         elif 2014 <= year <= 2022:
             header_year = "2014"
         else:
